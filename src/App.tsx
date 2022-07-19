243cd0d7adcb1aff8c842a4927f142b8e17e7bec
seggi SMS <seggimarugira@gmail.com>
2022-07-19 11:36:24 +0200

    add some changes in login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { Login } from './pages/auth/login';
+import { LoginPage } from './pages/auth/login';
+import { SignUpPage } from './pages/auth/signup';
 
 function App() {
+  const [showWidget, setShowWidget] = useState(false);
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <Login />
+        <div className="auth-box__row">
+          <div className="auth-content">
+            {showWidget !== false 
+            ? <SignUpPage />
+            : <LoginPage/>
+            }
+          </div>
+          <div className="change-widget-btn">
+            <button onClick={
+              () => {
+                setShowWidget(true)
+              }
+            }>
+              Sign up
+            </button>
+          </div>
+        </div>
+      
       </header>
     </div>
   );
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,26 +1,22 @@
 import { Link } from "react-router-dom";
 import "./index.css";
 
-export const Login =  () => {
+export const LoginPage =  () => {
     return (
         <>
-            <div className="auth-content">
-                <h2 className="auth-title">
-                    Note Add
-                </h2>
-                <div className="input-box">
-                    <form>
-                        <input type="text" value=""  placeholder="Enter Email"/>
-                        <input type="text" value=""  placeholder="Enter Password"/>
-                        <button>
-                            Login
-                        </button>
-                    </form>
-                </div>
-                <button className="switch-widget">
-                    You don't have account? click here
-                </button>
+            <h2 className="auth-title">
+                Note Add
+            </h2>
+            <div className="input-box">
+                <form>
+                    <input type="text" value=""  placeholder="Enter Email"/>
+                    <input type="text" value=""  placeholder="Enter Password"/>
+                    <button>
+                        Login
+                    </button>
+                </form>
             </div>
+           
         </>
     )
 }
